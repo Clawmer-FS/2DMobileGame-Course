e2b0049c256418ec5d39c1b5a0308adc3e0e1ab5
Juan <clawmer@gmail.com>
2020-09-16 15:24:24 +0200

    Section 5 - 44

--- 2DMobileGame-Course-2ndSecene/Assets/Scripts/Player.cs
+++ 2DMobileGame-Course-2ndSecene/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigid;
+    private PlayerAnimation _playerAnim;
+
 
     [Header("Player Movement")]
     [SerializeField]
@@ -13,10 +15,14 @@ public class Player : MonoBehaviour
     private float _speed = 2.5f;
     [SerializeField]
     private bool resetJump = false;
+
+    private SpriteRenderer _playerSprite;
     
     void Start()
     {
-        _rigid = GetComponent<Rigidbody2D>();   
+        _rigid = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<PlayerAnimation>();
+        _playerSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
@@ -27,13 +33,15 @@ public class Player : MonoBehaviour
     void Movement()
     {
         float move = Input.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
-
+        Flip(move);
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpforce);
             StartCoroutine(ResetJumpRoutine());
         }
+
+        _rigid.velocity = new Vector2(move * _speed, _rigid.velocity.y);
+        _playerAnim.Move(move);
     }
 
     bool IsGrounded()
@@ -47,6 +55,18 @@ public class Player : MonoBehaviour
             }            
         }
         return false;
+    }
+
+    void Flip(float move)
+    {
+        if (move > 0)
+        {
+            _playerSprite.flipX = false;
+        }
+        else if (move < 0)
+        {
+            _playerSprite.flipX = true;
+        }
     }
 
     IEnumerator ResetJumpRoutine()
