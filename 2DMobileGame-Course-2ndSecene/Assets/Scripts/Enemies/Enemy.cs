9c3b3f90b5b8d5b24e254382434dc61b24117ef3
Juan <clawmer@gmail.com>
2020-09-22 10:53:03 +0200

    Section 6 - 72

---
+++ 2DMobileGame-Course-2ndSecene/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [Header("Enemy Base Stats")]
+
+    [SerializeField]
+    protected int health;
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    protected int gems;
+
+    public virtual void Attack()
+    {
+
+    }
+
+    public abstract void Update();
+
+
+
+
+}
---
+++ 2DMobileGame-Course-2ndSecene/Assets/Scripts/Enemies/Spider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void Update()
+    { 
+
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+    }
+
+}
